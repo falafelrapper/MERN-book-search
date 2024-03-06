ca55bca64a72e0e24c9e7ce1e3d4ea881060f4f7
Zachary Roy <vgd199@gmail.com>
2024-03-05 16:43:08 -0800

    fixed entire code

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,5 @@
 const jwt = require('jsonwebtoken');
 const { GraphQLError } = require('graphql');
-
-// set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
@@ -11,16 +9,17 @@ module.exports = {
       code: 'UNAUTHENTICATED',
     },
   }),
-
   authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
+    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     try {
